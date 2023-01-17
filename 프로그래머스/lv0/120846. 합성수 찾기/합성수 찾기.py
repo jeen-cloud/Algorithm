6aa193d06d14ed5e0aef54c5d8060f1551fda093
jeen-cloud <113586234+jeen-cloud@users.noreply.github.com>
2023-01-17 22:50:51 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.py
@@ -3,6 +3,9 @@ def solution(n):
     for i in range(3, n+1):
         for j in range(2, i):
             if i % j == 0:
-                prime.append(i) 
-    answer = len(set(prime))
-    return answer
+                pass 
+            elif i % j != 0:
+                prime.append(i)
+        
+    answer = 0
+    return prime
